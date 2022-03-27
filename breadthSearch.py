459428d6c577b8935de00f59e3d46a8d8d4f4d5a
Joey <josiasdsj1@gmail.com>
2022-03-27 17:32:36 -0300

    Update search

--- breadthSearch.py
+++ breadthSearch.py
@@ -1,19 +1,46 @@
 #breadth search
 
-graphJar = [[]]
-stack = []
-vertexSize = 0
-passed = 0
-count = 0
+graph1 = {                          #Labirinto
+    "A": ["B","E"],
+    "B": ["A","C","F"],
+    "C": ["B"],
+    "E": ["A", "I"],
+    "I": ["E","J","N"],
+    "N": ["I"],
+    "J": ["I","O"],
+    "F": ["B","G"],
+    "G": ["F","H","L"],
+    "H": ["G","D"],
+    "D": ["H"],
+    "L": ["G","M"],
+    "M": ["L", "Q"],
+    "Q": ["M","P"],
+    "P": ["Q","O","S"],
+    "O": ["J","P","R"],
+    "R": ["O"],
+    "S": ["P"]
+}
+
+from pydoc import resolve
+import queue
+import collections
+
+def breadthSearch(graph, root):
+    visit, queue = set(),collections.deque([root])
+    visit.add(root)
+    while queue:
+        vert = queue.popleft()
+        print(("\0")+str(vert))
+        for near in graph[vert]:
+            if near not in visit:
+                queue.append(near)
+                visit.add(near)
 
 
-def breadth_search(graph, vertex):
-    stack = set()
-    while True:
-        vertex = queue.pop(0)
-        if vertex not in passed:
-            passed.add(vertex)
-            stack.extend(graph[vertex] - passed)
-        else:
-            break
-    return passed
+x = breadthSearch(graph1, "A")
+print(x)
+
+
+
+
+
