b476dc5e347c8c38220791ef27373450789e8d04
Josias da Silva Junior <josiasdsj1@gmail.com>
2022-05-01 12:42:19 -0300

    making new adjusts

--- target.py
+++ target.py
@@ -4,34 +4,36 @@ from time import sleep
 
 class Methods:
     def main(self):
+        nodes = ("A", "B", "C", "D", "E", "F", "G")
         graph = {
-        "A": {"B":9,"C":5,"D":13},
-        "B": {"A":9,"E":10,"D":3},
-        "C": {"A":5,"F":12},
-        "D": {"B":3,"E":6,"G":14},
-        "E": {"B":10,"D":6,"G":7},
-        "G": {"E":7,"F":10,"D":14},
-        "F": {"G":10,"C":12},
+        "A": {"B": 9, "C": 5, "D": 13},
+        "B": {"A": 9, "E": 10, "D": 3},
+        "C": {"A": 5, "F": 12},
+        "D": {"B": 3, "E": 6, "G": 14},
+        "E": {"B": 10, "D": 6, "G": 7},
+        "G": {"E": 7, "F": 10, "D": 14},
+        "F": {"G": 10, "C": 12},
     }
         print("Dijkstra")
         sleep(2)
-        x=self.recursiveDepthSearch(self,graph,[],"S")
-        x.reverse()
-        print(x)
-        print("===============")
-        print("Backtracking:")
-        sleep(1)
-        x=self.recursiveBacktracking(self,graph,"S","A",[])
-        x.reverse()
-        print(x)
-        print("===============")
-        print("Busca por largura:")
-        print(str(self.breadthSearch(self,graph,"A")))
+        x = self.recursiveDijkstra(self, graph, [], "S")
+
+    def recursiveDijkstra(self, graph):
+        while True:
+    for neighbour, distance in distances[current].items():
+        if neighbour not in unvisited: continue
+        newDistance = currentDistance + distance
+        if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
+            unvisited[neighbour] = newDistance
+    visited[current] = currentDistance
+    del unvisited[current]
+    if not unvisited: break
+    candidates = [node for node in unvisited.items() if node[1]]
+    current, currentDistance = sorted(candidates, key=lambda x: x[1])[0]
 
 
-    def recursiveDijkstra(self, graph,visited):
-
-            return 
+print(visited)
+            return
 
     def recursiveDepthSearch(self,graph, visited, node):
         if node not in visited:
