4c54d4e3d9c12e6e415729f7a3986ed1ed43ff59
Joey <josiasdsj1@gmail.com>
2022-05-02 12:09:02 -0300

    Update dijkstra before darkness consume my soul

--- dijkstra.py
+++ dijkstra.py
@@ -1,45 +1,77 @@
-from platform import node
 from time import sleep
 
+
 class Methods:
+
     def main(self):
-        nodes = ("A", "B", "C", "D", "E", "F", "G")
-        graph = {
-        "A": {      "B": 9,     "C": 5,     "D": 13     },
-        "B": {      "A": 9,     "D": 3,     "E": 10     },
-        "C": {      "F": 12,    "A": 5      },
-        "D": {      "A": 13,    "B": 3,     "E": 6,     "G": 14     },
-        "E": {      "B": 10,    "D": 6,     "G": 7      },
-        "G": {      "E": 7,     "F": 10,    "D": 14     },
-        "F": {      "C": 12,    "G": 10     },
-    }
         print("Dijkstra")
         sleep(2)
-        x = self.Dijkstra(self,graph,nodes,"A")
+        x = self.Dijkstra(self, "A", "G")
         print(x)
+        # print("Sistema especialista")
+        # sleep(2)
+        # x = self.especialista(self)
+        # print(x)
 
-        #Method
-    def Dijkstra(self,graph,nodes,start):
-        unvisited = {node:None for node in nodes}
+    def Dijkstra(self, start, end): 
+        #Define nós e grafo
+        nodes = ("A", "B", "C", "D", "E", "F", "G")
+        graph = {
+            "A": {
+                "B": 9,
+                "C": 5,
+                "D": 13
+            },
+            "B": {
+                "A": 9,
+                "D": 3,
+                "E": 10
+            },
+            "C": {
+                "F": 12,
+                "A": 5
+            },
+            "D": {
+                "A": 13,
+                "B": 3,
+                "E": 6,
+                "G": 14
+            },
+            "E": {
+                "B": 10,
+                "D": 6,
+                "G": 7
+            },
+            "G": {
+                "E": 7,
+                "F": 10,
+                "D": 14
+            },
+            "F": {
+                "C": 12,
+                "G": 10
+            },
+        }
+        unvisited = {n: float("inf") for n in nodes}
+        unvisited[start] = 0
         visited = {}
-        currentDistance = 0
-        unvisited[start]= currentDistance
-        while True:
-            for neighbour, distance in graph[start].items():
-                if neighbour not in unvisited:
+        parents = {}
+        while unvisited:
+            min_vertex = min(unvisited, key=unvisited.get)
+            for neighbour, _ in graph.get(min_vertex, {}).items():
+                if neighbour in visited:                        #continua a execusão se o vizinho foi visitado
                     continue
-                newDistance = currentDistance + distance
-                if unvisited[neighbour] is None or unvisited[neighbour] < newDistance:
-                    unvisited[neighbour] = newDistance
-                    visited[start] = currentDistance
-                    del unvisited[start]
-                    if not unvisited: 
-                        break
-                    candidates = [node for node in unvisited.items() if node[1]]
-                    start, currentDistance = sorted(candidates, key=lambda x: x[1])[0]
-            return visited
+                new_distance = unvisited[min_vertex] + graph[min_vertex].get(
+                    neighbour, float("inf"))
+                if new_distance < unvisited[neighbour]:
+                    unvisited[neighbour] = new_distance         #define a nova distancia 
+                    parents[neighbour] = min_vertex             #verifica o menor vertice/parente
+            visited[min_vertex] = unvisited[min_vertex]         
+            unvisited.pop(min_vertex)                           #remove da pilha
+            if min_vertex == end:
+                break
+        return visited
 
 
-x=Methods
+x = Methods
 x.main(x)
-
